bc7cb2c25747c2fd23f30ea90ac817a83c057640
Evgenii <workbox_1@tutanota.com>
2022-07-01 20:14:58 +0300

    Cycles nested in conditions

--- task_16.py
+++ task_16.py
@@ -1,11 +1,48 @@
 #!/usr/bin/python3
 
+from pickle import TRUE
 from pyrob.api import *
 
 
 @task
 def task_8_22():
-    pass
+
+    def corner():
+        if (wall_is_above() == True and 
+            wall_is_on_the_left() == True):
+            while wall_is_on_the_right() != True:
+                move_right()
+
+        elif (wall_is_above() == True and 
+              wall_is_on_the_right() == True):
+            while wall_is_on_the_left() != True:
+                move_left()
+
+        elif (wall_is_beneath() == True and 
+              wall_is_on_the_left() == True):
+            while wall_is_on_the_right() != True:
+                move_right()
+
+        elif (wall_is_beneath() == True and 
+              wall_is_on_the_right() == True):
+            while wall_is_on_the_left() != True:
+                move_left()
+    
+    if wall_is_above() != True:
+        while wall_is_above() != True:
+            move_up()
+        corner()
+    elif wall_is_beneath() != True:
+        while wall_is_beneath() != True:
+            move_down()
+        corner()
+    elif wall_is_on_the_left() != True:
+        while wall_is_on_the_left() != True:
+            move_left()
+        corner()
+    elif wall_is_on_the_right() != True:
+        while wall_is_on_the_right() != True:
+            move_right()
 
 
 if __name__ == '__main__':
--- task_18.py
+++ task_18.py
@@ -5,7 +5,51 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
+
+    def exit():
+        while wall_is_on_the_left() != True:
+            move_left()
+            if wall_is_above() != True:
+                break
+            elif wall_is_beneath() != True:
+                break 
+        if wall_is_on_the_left() == True:
+            while wall_is_on_the_right() != True:
+                move_right()
+                if wall_is_above() != True:
+                    break
+                elif wall_is_beneath() != True:
+                    break
+   
+
+    def move_to_wall_left():
+        while wall_is_on_the_left() != True:
+            move_left() 
+    
+    def move_to_wall_right():
+        while wall_is_on_the_right() != True:
+            move_right()
+
+    def move_to_wall_up():
+        while wall_is_above() != True:
+            move_up()
+    
+    def move_to_wall_down():
+        while wall_is_beneath() != True:
+            move_down()
+
+
+    exit()
+    if wall_is_above() != True:
+        move_to_wall_up()
+        move_to_wall_left()
+    elif wall_is_beneath() != True:
+        move_to_wall_down()
+        move_to_wall_left()
+        move_to_wall_up()
+
+
+
 
 
 if __name__ == '__main__':
