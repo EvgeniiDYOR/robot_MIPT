319243eca344616130ad1d288992e2eddc9b126a
Evgenii <workbox_1@tutanota.com>
2022-07-03 13:15:44 +0300

    Nested loops

--- task_20.py
+++ task_20.py
@@ -5,7 +5,24 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
+    def move_print_left(x):
+        for i in range(x):
+            fill_cell()
+            move_left() 
+        fill_cell()        
+    
+    def move_print_right(x):
+        for i in range(x):
+            fill_cell()
+            move_right()
+        fill_cell()
+
+    move_right()
+    for i in range(6):
+        move_print_right(26)
+        move_down()
+        move_print_left(26)
+        move_down()
 
 
 if __name__ == '__main__':
--- task_22.py
+++ task_22.py
@@ -5,7 +5,37 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-    pass
+
+    def print_to_wall_left():
+        fill_cell()
+        while wall_is_on_the_left() != True:
+            fill_cell()
+            move_left()
+        fill_cell()
+        if wall_is_beneath() != True:
+            move_down()
+    
+    def print_to_wall_right():
+        fill_cell()
+        while wall_is_on_the_right() != True:
+            fill_cell()
+            move_right()
+        fill_cell()
+        if wall_is_beneath() != True:
+            move_down()
+
+    for i in range(100):
+        print_to_wall_right()
+        if (wall_is_beneath() == True and
+            wall_is_on_the_right() == True):
+            print_to_wall_left()
+            break
+        if (wall_is_beneath() == True and
+            wall_is_on_the_left() == True):
+            print_to_wall_right()
+            print_to_wall_left()
+            break
+        print_to_wall_left()
 
 
 if __name__ == '__main__':
