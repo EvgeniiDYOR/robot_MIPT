ca1932b87dd029a84b703d93bb9b9aa47082b991
Evgenii <workbox_1@tutanota.com>
2022-07-03 15:49:59 +0300

    Creating and using subroutines

--- task_25.py
+++ task_25.py
@@ -5,7 +5,36 @@ from pyrob.api import *
 
 @task
 def task_2_2():
-    pass
+
+    flag = False
+    
+    def print_cross(flag):
+        for i in range(2):
+            fill_cell()
+            move_right()
+            fill_cell()
+            if wall_is_on_the_right():
+                flag = True
+        move_left()
+        move_up()
+        fill_cell()
+        move_down(2)
+        fill_cell()
+        return flag
+
+    move_down(2)
+    while flag != True:
+        flag = print_cross(flag)
+        if flag == True:
+            move_left()
+            move_up(2)
+            break
+        move_right(3)
+        move_up()
+
+
+    
+
 
 
 if __name__ == '__main__':
